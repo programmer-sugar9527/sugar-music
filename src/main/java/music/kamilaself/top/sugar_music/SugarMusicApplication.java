ef28cea8a6b32a068b6ca9d5d2bca1a7a2ef0c43
sugar <2424965116@qq.com>
2023-09-09 16:45:51 +0800

    second commit

---
+++ src/main/java/music/kamilaself/top/sugar_music/SugarMusicApplication.java
@@ -0,0 +1,13 @@
+package music.kamilaself.top.sugar_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SugarMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/music/kamilaself/top/sugar_music/SugarMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package music.kamilaself.top.sugar_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugarMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
